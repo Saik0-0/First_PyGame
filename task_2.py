f72a6fb86240bc2fdb01d98f29af4bb44feef84f
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-09-18 20:50:40 +0300

    step 7

--- task_2.py
+++ task_2.py
@@ -6,6 +6,9 @@ screen = pygame.display.set_mode((400, 300))
 
 done = False
 
+coord_left = (-100, -100)
+coord_right = (-100, -100)
+
 flag_blue = 0
 flag_red = 0
 
@@ -14,31 +17,33 @@ while not done:
         if event.type == pygame.MOUSEBUTTONDOWN:
 
             if event.button == 1:
-                coord_left = pygame.mouse.get_pos()
+                coord = pygame.mouse.get_pos()
+                coord_left = coord
+
+                if flag_blue == 1:
+                    screen.fill((0, 0, 0))
+                    pygame.draw.circle(screen, 'Blue', coord, 10)
+                    pygame.draw.rect(screen, 'Green', pygame.Rect(coord[0] - 5, coord[1] - 5, 10, 10))
+                    pygame.draw.circle(screen, 'Red', coord_right, 10)
+                else:
+                    flag_blue = 1
+
+                pygame.draw.circle(screen, 'Blue', coord, 10)
+                pygame.draw.rect(screen, 'Green', pygame.Rect(coord[0] - 5, coord[1] - 5, 10, 10))
+
+            if event.button == 3:
+                coord = pygame.mouse.get_pos()
+                coord_right = coord
 
                 if flag_blue == 1:
                     screen.fill((0, 0, 0))
                     pygame.draw.circle(screen, 'Blue', coord_left, 10)
                     pygame.draw.rect(screen, 'Green', pygame.Rect(coord_left[0] - 5, coord_left[1] - 5, 10, 10))
-                    # pygame.draw.circle(screen, 'Red', coord_right, 10)
+                    pygame.draw.circle(screen, 'Red', coord, 10)
                 else:
                     flag_blue = 1
 
-                pygame.draw.circle(screen, 'Blue', coord_left, 10)
-                pygame.draw.rect(screen, 'Green', pygame.Rect(coord_left[0] - 5, coord_left[1] - 5, 10, 10))
-
-            if event.button == 3:
-                coord_right = pygame.mouse.get_pos()
-
-                if flag_blue == 1:
-                    screen.fill((0, 0, 0))
-                    # pygame.draw.circle(screen, 'Blue', coord_left, 10)
-                    # pygame.draw.rect(screen, 'Green', pygame.Rect(coord_left[0] - 5, coord_left[1] - 5, 10, 10))
-                    pygame.draw.circle(screen, 'Red', coord_right, 10)
-                else:
-                    flag_blue = 1
-
-                pygame.draw.circle(screen, 'Red', coord_right, 10)
+                pygame.draw.circle(screen, 'Red', coord, 10)
 
         if event.type == pygame.QUIT:
             done = True
