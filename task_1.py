6f5e97346f0a5ed5a4fe9f43f0ce91ead98fb8d4
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-09-18 13:46:12 +0300

    step 4

--- task_1.py
+++ task_1.py
@@ -13,17 +13,22 @@ done = False
 
 while not done:
     for event in pygame.event.get():
-
         if event.type == pygame.MOUSEBUTTONDOWN:
             screen.fill((0, 0, 0))
             coord = pygame.mouse.get_pos()
-            pygame.draw.circle(screen, 'Pink', (coord[0], 300), 15)
+            pygame.draw.circle(screen, 'Pink', (coord[0], 300), 8)
             for i in range(300 - coord[1]):
                 screen.fill((0, 0, 0))
-                pygame.draw.circle(screen, 'Pink', (coord[0], 300 - i), 15)
+                pygame.draw.circle(screen, 'Pink', (coord[0], 300 - i), 8)
                 pygame.display.flip()
                 clock.tick(240)
-            pygame.draw.circle(screen, 'Red', coord, 25)
+
+            pygame.draw.circle(screen, 'Red', coord, 15)
+            pygame.display.flip()
+            pygame.time.delay(250)
+
+            screen.fill((0, 0, 0))
+            pygame.display.flip()
 
         if event.type == pygame.QUIT:
             done = True
